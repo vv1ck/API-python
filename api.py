1bca38887a61308f872bb0a64e684a4a284ea5e0
vv1ck <75469718+vv1ck@users.noreply.github.com>
2021-09-30 01:51:16 +0300

    Update api.py

--- api.py
+++ api.py
@@ -1,15 +1,17 @@
 from flask import Flask
+import json
 from requests import post
-api=Flask(__name__)
+app=Flask(__name__)
 
 
-@api.route('/email=<email>')
+@app.route('/email=<email>')
 def api_email(email):
-    send=post(f'https://odc.officeapps.live.com/odc/emailhrd/getidp?hm=0&emailAddress={email}',headers={
-        "Host": "odc.officeapps.live.com","Accept": "*/*",
-        "Cache-Control": "max-age=0",
-        "Accept-Encoding": "gzip, deflate",
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0"}).text
-    if 'MSAccount' in send:return '{"by telegram:@vv1ck" , status: True}'
-    else:return '{"by telegram:@vv1ck" , status: False}'
-api.run()
+	send=post(f'https://odc.officeapps.live.com/odc/emailhrd/getidp?hm=0&emailAddress={email}',headers={
+		"Host": "odc.officeapps.live.com","Accept": "*/*",
+		"Cache-Control": "max-age=0",
+		"Accept-Encoding": "gzip, deflate",
+		"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0"}).text
+	if 'MSAccountNonEmail' in send:return 'This is not an email'
+	elif 'MSAccount' in send:return '{"by telegram:@vv1ck" , status: True}'
+	else:return '{"by telegram:@vv1ck" , status: False}'
+app.run()
